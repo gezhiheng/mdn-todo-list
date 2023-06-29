3490d1fd9cf99cc530a11dc99b31bdb2a34d1112
Henry Ge <1981761687@qq.com>
2023-06-29 15:33:24 +0800

    第一次提交

---
+++ src/components/ToDoForm.vue
@@ -0,0 +1,37 @@
+<template>
+  <form @submit.prevent="onSubmit">
+    <label for="new-todo-input" class="label__lg">
+      What needs to be done?
+    </label>
+    <input 
+      type="text"
+      id="new-todo-input"
+      name="new-to-do"
+      autocomplete="off"
+      v-model.lazy.trim="userInput"
+      class="input__lg"
+      >
+      <button type="submit" class="btn btn__primary btn__lg">
+        Add
+      </button>
+  </form>
+</template>
+
+<script>
+  export default {
+    methods: {
+      onSubmit() {
+        if(this.userInput === "") {
+          return;
+        }
+        this.$emit("todo-added", this.userInput);
+        this.userInput = "";
+      }
+    },
+    data() {
+      return {
+        userInput: ""
+      }
+    }
+  }
+</script>
